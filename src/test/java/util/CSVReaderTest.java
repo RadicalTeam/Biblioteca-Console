37d6392ffc8ad93c1279b047bb087bbc30046e58
luopiao <piluo@thoughtworks.com>
2017-10-03 12:14:23 +0800

    [LuoPiao] add csvReader test

---
+++ src/test/java/util/CSVReaderTest.java
@@ -0,0 +1,34 @@
+package util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class CSVReaderTest {
+    private CSVReader csvReader;
+    @Before
+    public void setUp() {
+        csvReader = new CSVReader();
+    }
+    @Test
+    public void should_return_book_detail_when_given_a_valid_book_name() throws Exception {
+        Map<String, String> exceptBookDetail = new HashMap<>();
+        exceptBookDetail.put("bookName", "Java Start");
+        exceptBookDetail.put("author", "Jackson");
+        exceptBookDetail.put("publishYear", "2007");
+        exceptBookDetail.put("totalQuantity", "20");
+        exceptBookDetail.put("remainQuantity", "2");
+        assertEquals(csvReader.findBookDetailByBookName("java start"), exceptBookDetail);
+    }
+
+    @Test
+    public void should_return_empty_map_when_given_a_invalid_book_name() {
+        Map<String, String> exceptBookDetail = new HashMap<>();
+        assertEquals(csvReader.findBookDetailByBookName("PHP Start"), exceptBookDetail);
+    }
+
+}
